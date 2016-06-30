0ebc1d273f5fcd11bab2cc99880cea21e0d453b2
Kayvin <kayvin4@hotmail.com>
2016-06-30 10:42:00 +0200

    fwrite
    Création fichier log

--- index.php
+++ index.php
@@ -4,4 +4,11 @@ $file = fopen("texte.txt", "r");
 var_dump(fread($file, filesize("texte.txt")));
 fclose($file);
 //Utiliser les 3 fonctions systématiquement
+
+//Pour ecrire dans un fichier
+$file = fopen("texte.log", "a");
+fwrite($file, date("Y-m-d-H:i:s")." : Le script a été ouvert\r\n");
+fclose($file);
+
+
 ?>
